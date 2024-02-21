d59507c3f6d7cd00af746b04a9cb2c39de3664f6
maksie <dimedmax95@gmail.com>
2024-02-21 18:11:43 +0400

    3 task

---
+++ t3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход целое число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = 23;
+
+int first_digit = num / 10;
+int second_digit = num % 10;
+
+if (first_digit > second_digit){
+    Console.WriteLine ( first_digit + " наибольшая цифра");
+}
+else if (second_digit > first_digit){
+    Console.WriteLine ( second_digit + " наибольшая цифра");
+}
