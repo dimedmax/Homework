32d6f006e06a72e642dd0da806e7766b82b31af7
maksie <dimedmax95@gmail.com>
2024-02-21 18:11:57 +0400

    4 task

---
+++ t4/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая на вход принимает натуральное число N
+// , а на выходе показывает его цифры через запятую.
+
+int number = 23;
+
+int first_digit = number / 10;
+int last_digit = number % 10;
+
+Console.WriteLine(first_digit +","+ last_digit);
